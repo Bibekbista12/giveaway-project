b7f2a3cc4f767a9b297bc4f856725e66e2900fbc
Bibekbista12 <Bibekbista12@gmail.com>
2026-06-23 22:44:34 +0545

    Switch to Brevo SMTP for real email delivery

--- giveaway_project/settings.py
+++ giveaway_project/settings.py
@@ -139,3 +139,11 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+SITE_BASE_URL = "https://giveaway-project.vercel.app"
